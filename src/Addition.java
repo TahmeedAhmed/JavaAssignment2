0b9ca18fecaaa4cd89366b64d9a7aca24895e702
NTTData <tahmeed.ahmed@nttdata.com>
2020-03-05 13:00:07 +0000

    finished task

---
+++ src/Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        System.out.println(addTwo(5, 20));
+    }
+
+    public static int addTwo(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/PrimitiveDataTypes.java
@@ -0,0 +1,17 @@
+public class PrimitiveDataTypes {
+
+    static boolean check = true;
+    static byte myByte = 127;
+    static char myChar = 's';
+    static short myShort = 30000;
+    static int myInt = 2147483647;
+    static long myLong = 922337203685477580L;
+    static float myFloat = 69f;
+    static double myDouble = 100.024;
+
+    public static void main(String[] args) {
+        // boolean , byte , char , short , int , long , float and double
+        int a = 343243;
+        System.out.println(Integer.max(a, myInt));
+    }
+}
